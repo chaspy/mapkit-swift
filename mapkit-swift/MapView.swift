cade6a71fac5e7287f161d654c94f180b656103c
kondo takeshi <chaspy@users.noreply.github.com>
2023-09-03 10:48:39 +0900

    feat: Add search functionality to MapView

--- mapkit-swift/MapView.swift
+++ mapkit-swift/MapView.swift
@@ -9,17 +9,19 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    let searchKey: String
+    
     func makeUIView(context: Context) -> MKMapView {
         MKMapView()
     }
     
     func updateUIView(_ uiView: MKMapView, context: Context){
-        
+        print("検索キーワード:\(searchKey)")
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(searchKey: "羽田空港")
     }
 }
